415a07dafc7b8651aa8ccd405c679a196aa95080
root <rph322@gmail.com>
2018-09-08 11:28:47 +0200

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -55,7 +55,7 @@ if [ ! -f /etc/ocserv/certs/server-key.pem ] || [ ! -f /etc/ocserv/certs/server-
 	# Create a test user
 	if [ -z "$NO_TEST_USER" ] && [ ! -f /etc/ocserv/ocpasswd ]; then
 		echo "Create test user 'vpn' with password 'vpn'"
-                echo "vpn" | ocpasswd -c /etc/ocserv/ocpasswd "vpn"
+                echo "${PASSWORD:-vpn}" | ocpasswd -c /etc/ocserv/ocpasswd "${USERNAME:-vpn}"
 	fi
 fi
 
@@ -64,12 +64,12 @@ sysctl -w net.ipv4.ip_forward=1
 
 # Enable NAT forwarding
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Enable TUN device
+if [ ! -e /dev/net/tun ]; then
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
-
+fi
 # Run OpennConnect Server
 exec "$@"
